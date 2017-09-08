2e7f180f099d1f85daa0fd7c38305d3d5341158f
Z.Tokbayev <Z.Tokbayev@TERMINAL-6.net.ets>
2017-09-08 16:56:35 +0600

    080920171

--- discoveries/src/main/java/kz/ztokbayev/greetgo/discoveries/App.java
+++ discoveries/src/main/java/kz/ztokbayev/greetgo/discoveries/App.java
@@ -3,6 +3,7 @@ package kz.ztokbayev.greetgo.discoveries;
 import org.apache.log4j.Logger;
 import org.apache.log4j.PropertyConfigurator;
 import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import java.io.IOException;
@@ -13,13 +14,12 @@ import java.util.List;
 
 public class App 
 {
-	//final static Logger logger = Logger.getLogger(App.class);
-	
+	final static Logger logger = Logger.getLogger(App.class);
+	 
     public static void main( String[] args )
     {
-    	//PropertyConfigurator.configure("/resources/log4j.properties");
-    	//logger.info("Classification: " + Classification.id1);
-    	
+    	//PropertyConfigurator.configure("log4j.properties");
+    	    	
 		/*for (Classification cName : Classification.values()) {
 			System.out.println(cName + ") " + cName.getValue());
 		}*/
@@ -29,16 +29,34 @@ public class App
 		SqlSessionFactory sqlSessionFactory;
         DiscovererMapper discovererMapper;
         Reader reader = null;
+        SqlSession session = null;
+        
         try {
             reader = Resources.getResourceAsReader("mybatis-config.xml");
             sqlSessionFactory = new SqlSessionFactoryBuilder().build(reader);
-            discovererMapper = sqlSessionFactory.openSession().getMapper(DiscovererMapper.class); //Создаем маппер, из которого и будем вызывать методы getSubscriberById и getSubscribers
+           
+            session = sqlSessionFactory.openSession();
+            discovererMapper = session.getMapper(DiscovererMapper.class); //Создаем маппер, из которого и будем вызывать методы getSubscriberById и getSubscribers
             List <Discoverer> discoverers = discovererMapper.getDiscoverers();
-            System.out.println("Firstname " + discoverers.get(0).getFirstname());
+            //System.out.println("Firstname " + discoverers.get(0).getFirstname());
             Discoverer discoverer = discovererMapper.getDiscovererById(1);
-            System.out.println("Firstname " + discoverer.getFirstname());
+            //System.out.println("Firstname " + discoverer.getFirstname());
+            discoverer.setFirstname("Александр");
+            //System.out.println(discoverer.getFirstname());
+            //discovererMapper.updateDiscoverer(discoverer);
+            Discoverer dsc = new Discoverer(0, "Георгий", "Авасян", "Мишелович");
+            discovererMapper.addDiscoverer(dsc);
+            session.commit();
+            discoverer = discovererMapper.getDiscovererById(1);
+            
+            logger.info("Firstname " + discoverer.getFirstname());
         } catch (IOException e) {
             e.printStackTrace();
+        }
+        finally {
+        	 if (session != null) {
+                 session.close();
+             }
         }
     }
 }
--- discoveries/src/main/java/kz/ztokbayev/greetgo/discoveries/DiscovererMapper.java
+++ discoveries/src/main/java/kz/ztokbayev/greetgo/discoveries/DiscovererMapper.java
@@ -1,11 +1,12 @@
 package kz.ztokbayev.greetgo.discoveries;
 
 import java.util.List;
-
 import kz.ztokbayev.greetgo.discoveries.Discoverer;
 
 public interface DiscovererMapper {
 	
-	Discoverer getDiscovererById(Integer id);
 	List<Discoverer> getDiscoverers();
+	Discoverer getDiscovererById(Integer id);
+	void updateDiscoverer(Discoverer dsc);
+	void addDiscoverer(Discoverer dsc);
 }
