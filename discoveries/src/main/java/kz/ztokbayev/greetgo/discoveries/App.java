c66299cc49db8faae6e5ee04ecd4025397f0e8c6
Z.Tokbayev <Z.Tokbayev@TERMINAL-6.net.ets>
2017-09-14 16:50:10 +0600

    140920171650

--- discoveries/src/main/java/kz/ztokbayev/greetgo/discoveries/App.java
+++ discoveries/src/main/java/kz/ztokbayev/greetgo/discoveries/App.java
@@ -4,36 +4,13 @@ import org.apache.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import kz.ztokbayev.greetgo.discoveries.db.DatabaseManager;
-
 @SpringBootApplication
 public class App 
 {
 	public static Logger logger = Logger.getLogger(App.class);
-	public static DatabaseManager dbmanager = new DatabaseManager();
-	
 	
     public static void main( String[] args )
-    {
-		/*for (Classification cName : Classification.values()) {
-			System.out.println(cName + ") " + cName.getValue());
-		}*/
-        //logger.info("Firstname " + discoverers.get(0).getFirstname());
-            
-        //Discoverer discoverer = discovererMapper.getDiscovererById(1);
-        //System.out.println("Firstname " + discoverer.getFirstname());
-        //discoverer.setFirstname("Александр");
-        //System.out.println(discoverer.getFirstname());
-        //discovererMapper.updateDiscoverer(discoverer);
-        //Discoverer dsc = new Discoverer(0, "Георгий", "Авасян", "Мишелович");
-        //discovererMapper.addDiscoverer(dsc);
-        //session.commit();
-        //discoverer = discovererMapper.getDiscovererById(1);
-        
-        //logger.info("Firstname " + discoverer.getFirstname());
-        
+    	{
         SpringApplication.run(App.class, args);
-
-        //dbmanager.CloseSession();
         }
 }
--- discoveries/src/main/java/kz/ztokbayev/greetgo/discoveries/web/RestWebController.java
+++ discoveries/src/main/java/kz/ztokbayev/greetgo/discoveries/web/RestWebController.java
@@ -2,28 +2,26 @@ package kz.ztokbayev.greetgo.discoveries.web;
 
 import java.util.List;
 
-import org.springframework.boot.json.JacksonJsonParser;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.fasterxml.jackson.core.JsonParser;
-import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.util.JSONPObject;
 
-import kz.ztokbayev.greetgo.discoveries.App;
 import kz.ztokbayev.greetgo.discoveries.model.Discoverer;
+import kz.ztokbayev.greetgo.discoveries.db.DatabaseManager;
 import kz.ztokbayev.greetgo.discoveries.model.Star;
 
 @RestController
 public class RestWebController {
 		
+	private DatabaseManager dbmanager = new DatabaseManager();
+	
 	@RequestMapping(value = "discoverers.html/getdiscoverers", method = RequestMethod.GET)
 	public Response getDiscoverers() {
-		List<Discoverer> discoverers = App.dbmanager.GetAllDiscoverers();
+		List<Discoverer> discoverers = dbmanager.GetAllDiscoverers();
 		Response response = new Response("Done", discoverers);
 		return response;
 	}
@@ -34,7 +32,7 @@ public class RestWebController {
 		ObjectMapper objectMapper = new ObjectMapper();
 		try {
 			Discoverer discoverer = objectMapper.readValue(jsonString, Discoverer.class);
-			App.dbmanager.addDiscoverer(discoverer);
+			dbmanager.addDiscoverer(discoverer);
 		}
 		catch (Exception e)
 		{
@@ -46,14 +44,14 @@ public class RestWebController {
 	
 	@RequestMapping(value = "discoverers.html/deldiscoverer", method = RequestMethod.POST)
 	public Response delDiscoverer(@RequestParam(value="id") Integer id) {
-		App.dbmanager.delDiscovererById(id);
+		dbmanager.delDiscovererById(id);
 		Response response = new Response("Done", null);
 		return response;
 	}
 	
 	@RequestMapping(value = "discoverers.html/getdiscoverer", method = RequestMethod.GET)
 	public Response getDiscovererById(@RequestParam(value="id") Integer id) {
-		Discoverer discoverer = App.dbmanager.getDiscovererById(id);
+		Discoverer discoverer = dbmanager.getDiscovererById(id);
 		Response response = new Response("Done", discoverer);
 		return response;
 	}
@@ -64,7 +62,7 @@ public class RestWebController {
 		ObjectMapper objectMapper = new ObjectMapper();
 		try {
 			Discoverer discoverer = objectMapper.readValue(jsonString, Discoverer.class);
-			App.dbmanager.updateDiscoverer(discoverer);
+			dbmanager.updateDiscoverer(discoverer);
 		}
 		catch (Exception e)
 		{
@@ -76,7 +74,7 @@ public class RestWebController {
 	
 	@RequestMapping(value = "/getstars", method = RequestMethod.GET)
 	public Response getStars() {
-		List<Star> stars = App.dbmanager.GetStars();
+		List<Star> stars = dbmanager.GetStars();
 		Response response = new Response("Done", stars);
 		return response;
 	}
